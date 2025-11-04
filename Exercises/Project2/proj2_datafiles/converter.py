eb50a8cd1598f9a186c4be5e72763fbf376c63c5
Vicente Ibanez <vincentibanez2001@gmail.com>
2025-11-03 19:04:07 -0500

    Final Commit

--- Exercises/Project2/proj2_datafiles/converter.py
+++ Exercises/Project2/proj2_datafiles/converter.py
@@ -21,15 +21,15 @@ class IFToPFConverter(object):
         self.current_token = None
 
     def __str__(self):
-        """Returns a string respentation of the postfix expression.
+        """Returns a string respresentation of the postfix expression.
         Returns the portion of the expression that was converted so far.
         Also returns the operators still on the stack.
         """
         rv = "\n"
         if len(self.expressionSoFar) != 0:
-            rv = "Expression so far: " + "".join(map(lambda esf: str(esf.getValue()) + " ", self.expressionSoFar)) + "\n"
+            rv = "Portion of expression processed: " + "".join(map(lambda esf: str(esf.getValue()) + " ", self.expressionSoFar)) + "\n"
         else:
-            rv = "Expression so far: none\n" 
+            rv = "Portion of expression processed: none\n" 
 
         if self.operatorStack.isEmpty():
             rv += "The stack is empty"
@@ -44,15 +44,14 @@ class IFToPFConverter(object):
         It can be used to assist with error handling."""
         rv = "\n"
         if len(self.expressionSoFar) != 0:
-            rv = "Expression so far: " + "".join(map(lambda esf: str(esf.getValue()) + " ", self.expressionSoFar)) + "\n"
+            rv = "Portion of expression processed: " + "".join(map(lambda esf: str(esf.getValue()) + " ", self.expressionSoFar)) + "\n"
         else:
-            rv = "Expression so far: none\n" 
+            rv = "Portion of expression processed: none\n" 
 
         if self.operatorStack.isEmpty():
-            print("The stack is empty")
             rv += "The stack is empty"
         else:
-            rv += "Operators on stack: " + "".join(map(lambda os: str(os.getValue()) + " ", self.operatorStack))
+            rv += "Operators on stack: " + str([x.getValue() for x in self.operatorStack])
 
         return rv
 
@@ -92,6 +91,9 @@ class IFToPFConverter(object):
         stack = ArrayStack()
         self.operatorStack = stack
 
+        numOperators = 0
+        numOperands = 0
+
         while self.scanner.hasNext(): # Step 2
             currentToken = self.scanner.next()
             self.current_token = currentToken
@@ -100,28 +102,36 @@ class IFToPFConverter(object):
                 raise AttributeError("Unrecognized symbol")
             elif currentToken.getType() == Token.INT: # Step 3
                 postfix.append(currentToken)
+                numOperands += 1
             elif currentToken.getType() == Token.LPAR: # Step 4
                 stack.push(currentToken)
             elif currentToken.getType() == Token.RPAR: # Step 6
+                if stack.isEmpty():
+                    raise AttributeError("Too few operators")
                 topOperator = stack.pop()
                 while topOperator.getType() != Token.LPAR:
                     postfix.append(topOperator)
+                    if stack.isEmpty():
+                        # no left parenthesis
+                        raise AttributeError("Too few operators")
                     topOperator = stack.pop()
             elif currentToken.isOperator(): # Step 5
-                # TO DO: If the operatorStack is empty raise an AttributeError: Too few operators
+                numOperators += 1
+                if len(postfix) == 0:
+                    raise AttributeError("Too few operators")
                 while (not stack.isEmpty()) \
-                    and stack.peek().getPrecedence() >= currentToken.getPrecedence() and currentToken.getType() != Token.EXPO:
-                    if stack.isEmpty():
-                        raise AttributeError("Too few operators")
+                    and stack.peek().getPrecedence() >= currentToken.getPrecedence() \
+                    and currentToken.getType() != Token.EXPO:
                     postfix.append(stack.pop())
-                    
 
                 stack.push(currentToken)
             
-
-
         while not stack.isEmpty():
             postfix.append(stack.pop())
+
+        if numOperands - numOperators != 1:
+            raise AttributeError("Too few operators")
+        
         return postfix
    
 
@@ -140,13 +150,65 @@ def main():
             postfix = converter.convert()
             print("Postfix expression: ", "".join(map(lambda pf: str(pf.getValue()) + " ", postfix)))
         except Exception as e:
-            print(e, end=". ")
-            print(converter.conversionStatus(), end=". ")
-            print("The token that caused the error:", str(converter.current_token.getValue()))
+            print(e)
+            print(converter.conversionStatus())
+            # print("The token that caused the error:", str(converter.current_token.getValue()))
 
+
+def testMain(testCase):
+    """This function acts line the main function for the test() function."""
+    try:
+        scanner = Scanner(testCase)
+        converter = IFToPFConverter(scanner)
+        postfix = converter.convert()
+        print("Postfix expression: ", "".join(map(lambda pf: str(pf.getValue()) + " ", postfix)))
+    except Exception as e:
+        print(e)
+        print(converter.conversionStatus())
+        raise e
+
+def test():
+    """ A tester function for convert.py"""
+    test1 = "3 * 0"
+    test2 = "3 4 2 5"
+    test3 = "5 + 6 - 3 & 9"
+    test4 = "2 $ 3"
+    test5 = "2$"
+    try:
+        testMain(test1)
+        print("Test 1 Passed")
+    except Exception as e:
+        print("Test 1 Failed", e)
+
+    try:
+        testMain(test2)
+        print("Test 2 Failed")
+    except Exception as e:
+        print("Test 2 Passed", e)
+
+    try:
+        testMain(test3)
+        print("Test 3 Failed")
+    except Exception as e:
+        print("Test 3 Passed", e)
+
+    try:
+        testMain(test4)
+        print("Test 4 Failed")
+    except Exception as e:
+        print("Test 4 Passed", e)
+    
+    try:
+        testMain(test5)
+        print("Test 5 Failed")
+    except Exception as e:
+        print("Test 5 Passed", e)
+
+    print("The test above made sure postfix was working as expected and tested every exception.")
 
 
 if __name__ == "__main__":
     main()
+    # test()
 
 
